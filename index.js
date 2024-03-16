c073de81f074f66451b93d7d6241914df5e4d2e4
soomni <lovesoomindl@naver.com>
2024-03-16 15:29:59 -0400

    added

--- index.js
+++ index.js
@@ -50,7 +50,11 @@ sourceHTML_inter = replacedHTML_inter;
 
 
 const dir = './data';
-
+fs.readdir(dir, (err, files) => {
+  console.log(files.length);
+  personal_id = files.length;
+  var stream = fs.createWriteStream("data/"+personal_id+".txt");
+});
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
@@ -209,11 +213,7 @@ app.post(['/lastPage'], function(req, res) {
     }
 });
 
-fs.readdir(dir, (err, files) => {
-  console.log(files.length);
-  personal_id = files.length;
-  var stream = fs.createWriteStream("data/"+personal_id+".txt");
-});
+
   function readWrite(input_data){
     fs.appendFileSync("data/"+personal_id+".txt",String(input_data.gender)+" "+ String(input_data.age)+"\n");
   }
