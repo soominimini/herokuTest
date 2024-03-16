5e95802d56438cd6d54656d885682f0d5384a115
soomni <lovesoomindl@naver.com>
2024-03-16 15:15:37 -0400

    added

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ app.post(['/lastPage'], function(req, res) {
 fs.readdir(dir, (err, files) => {
   console.log(files.length);
   personal_id = files.length;
-  var stream = fs.createWriteStream(personal_id+".txt");
+  var stream = fs.createWriteStream("data/"+personal_id+".txt");
 });
   function readWrite(input_data){
     fs.appendFileSync("data/"+personal_id+".txt",String(input_data.gender)+" "+ String(input_data.age)+"\n");
